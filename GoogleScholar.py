132d95cc6f0ce73cb150e58d74212ef1f240ac62
Stephen Makonin <stephen@makonin.com>
2019-03-06 12:50:13 -0800

    refactor

---
+++ GoogleScholar.py
@@ -0,0 +1,124 @@
+# Simple libreary  to process Google Scholar Citations
+# Copyright (C) 2016 Stephen Makonin. All Right Reserved.
+#
+
+
+import urllib.request, re, datetime, html as HTML
+from html.parser import HTMLParser
+from pprint import pprint
+from dataclasses import dataclass, asdict, astuple
+
+
+journals = [['Transactions on Smart Grid'             , 'TSG'    , 7.364],
+            ['Pervasive Computing'                    , 'PvC'    , 3.022],
+            ['Energy Efficiency'                      , 'ENEF'   , 1.634],
+            ['Journal on Computing'                   , 'JoC'    , 'tbd'],
+            ['Scientific Data'                        , 'SData'  , 4.836],
+            ['Applied Energy'                         , 'APEN'   , 7.900],
+            ['Data in Brief'                          , 'DIB'    , 1.430],
+            ['Transactions on Circuits and Systems II', 'TCAS-II', 2.450],
+            ['MDPI Data'                              , 'DATA'   , 'tbd']]
+
+books =    ['Sams Publishing',
+            'Woodhead Publishing']
+
+scholar_url = 'https://scholar.google.ca/citations?user=%s&hl=en&pagesize=999'
+
+keys = ['Citations',
+        'h-index',
+        'i10-index']
+
+
+def get_chunk(html, pre, post):
+    start = html.find(pre) + len(pre)
+    end = start + html[start:].find(post)
+    return html[start:end]
+
+def get_number(html, keyword, pre='<tr>', post='</tr>'):
+    idx = html.rfind(keyword)
+    start = html[:idx].rfind(pre) + 4
+    end = idx + html[idx:].find(post)
+    row = html[start:end]
+    columns = row.replace('</td>', '</td>\n').split('\n')
+    num = int(re.sub('<[^<]+?>', '', columns[1]))
+    return num
+
+def get_if(text):
+    for journal in journals:
+        if text.find(journal[0]) > -1:
+            return journal[2]
+
+    return ''
+
+def is_book(text):
+    for book in books:
+        if text.find(book) > -1:
+            return True
+
+    return False
+
+
+@dataclass
+class PaperData:
+    id: str
+    title: str
+    citations: int
+    is_conference: bool
+    is_journal: bool
+    is_book: bool
+    impact_factor: str
+
+
+###EXAMPLE: get specific paper citations
+#papers = {'cneuo_UAAAAJ:zYLM7Y9cAGgC': 'AMPds: A public dataset for load disaggregation and eco-feedback research',
+#          'cneuo_UAAAAJ:UebtZRa9Y70C': 'Visual C++ 5.0 Developer\'s Guide',
+#          'cneuo_UAAAAJ:LkGwnXOMwfcC': 'Transmitting Patient Vitals Over a Reliable ZigBee Mesh Network'}
+#
+#for key in papers:
+#    print('%-50s citations = %5s' % (papers[key][:50], get_number(html, key, row_start='<tr class="gsc_a_tr">')))
+
+def get_scholar_data(id):
+    agg = []
+    data = []
+
+    response = urllib.request.urlopen(scholar_url % id)
+    html = response.read().decode(response.headers.get_content_charset())
+    response.close()
+
+    papers_html = get_chunk(html, '<tbody id="gsc_a_b">', '</tbody>')
+    papers_html = papers_html.replace('<tr class="gsc_a_tr">', '')
+    papers_html = papers_html.replace('</tr>', '\n')
+    papers = papers_html[:-1].split('\n')
+    hparser = HTMLParser()
+
+    for key in keys:
+        agg.append('%-9s = %7s' % (key, format(get_number(html, key), ',d')))
+
+    for paper in papers:
+        key = 'citation_for_view='
+        start = paper.find(key) + len('citation_for_view=')
+        end = start + paper[start:].find('"')
+        id = paper[start:end]
+
+        paper = [v for v in re.sub('<[^<]+?>', '|', paper).split('|') if v]
+        if len(paper) == 0:
+            continue
+
+        if len(paper) < 6:
+            count = 0
+        else:
+            try:
+                count = int(paper[4])
+            except:
+                count = 0
+
+        name = HTML.unescape(paper[0])
+        impact = get_if(paper[2])
+        a_book = is_book(paper[2])
+
+        data.append(PaperData(id, name, count, (impact == '' and not a_book), (impact != ''), a_book, impact))
+    return (agg, data)
+
+if __name__ == "__main__":
+    for data in get_scholar_data('cneuo_UAAAAJ'):
+        pprint(asdict(data), indent=4, width=160)
--- ProcessCitationsPage.py
+++ ProcessCitationsPage.py
@@ -4,64 +4,9 @@
 # Copyright (C) 2016 Stephen Makonin. All Right Reserved.
 #
 
-import urllib.request, re, datetime, html as HTML
-from html.parser import HTMLParser
 
-
-scholar_url = 'https://scholar.google.ca/citations?user=cneuo_UAAAAJ&hl=en&pagesize=999'
-
-
-journals = [['Transactions on Smart Grid'             , 'TSG'    , 7.364],
-            ['Pervasive Computing'                    , 'PvC'    , 3.022],
-            ['Energy Efficiency'                      , 'ENEF'   , 1.634],
-            ['Journal on Computing'                   , 'JoC'    , 'tbd'],
-            ['Scientific Data'                        , 'SData'  , 4.836],
-            ['Applied Energy'                         , 'APEN'   , 7.900],
-            ['Data in Brief'                          , 'DIB'    , 1.430],
-            ['Transactions on Circuits and Systems II', 'TCAS-II', 2.450],
-            ['MDPI Data'                              , 'DATA'   , 'tbd']]
-
-books =    ['Sams Publishing',
-            'Woodhead Publishing']
-
-
-def get_chunk(html, pre, post):
-    start = html.find(pre) + len(pre)
-    end = start + html[start:].find(post)
-    return html[start:end]
-
-def get_number(html, keyword, pre='<tr>', post='</tr>'):
-    idx = html.rfind(keyword)
-    start = html[:idx].rfind(pre) + 4
-    end = idx + html[idx:].find(post)
-    row = html[start:end]
-    columns = row.replace('</td>', '</td>\n').split('\n')
-    num = int(re.sub('<[^<]+?>', '', columns[1]))
-    return num
-
-def get_if(text):
-    global impact_total
-    for journal in journals:
-        if text.find(journal[0]) > -1:
-            if journal[2] != 'tbd':
-                impact_total += journal[2]
-                return '%10.03f' % (journal[2])
-            else:
-                return '%10s' % (journal[2])
-
-    return ''
-
-def is_book(text):
-    for book in books:
-        if text.find(book) > -1:
-            return True
-
-    return False
-
-
-response = urllib.request.urlopen(scholar_url)
-html = response.read().decode(response.headers.get_content_charset())
-response.close()
+import datetime
+from GoogleScholar import get_scholar_data
 
 
 print()
@@ -69,29 +14,11 @@ print('Report generated on:', datetime.datetime.now())
 
 print()
 
-keys = ['Citations',
-        'h-index',
-        'i10-index']
+(agg, papers) = get_scholar_data('cneuo_UAAAAJ')
 
-for key in keys:
-    print('%-9s = %7s' % (key, format(get_number(html, key), ',d')))
-
+for line in agg:
+    print(line)
 print()
-
-###EXAMPLE: get specific paper citations
-#papers = {'cneuo_UAAAAJ:zYLM7Y9cAGgC': 'AMPds: A public dataset for load disaggregation and eco-feedback research',
-#          'cneuo_UAAAAJ:UebtZRa9Y70C': 'Visual C++ 5.0 Developer\'s Guide',
-#          'cneuo_UAAAAJ:LkGwnXOMwfcC': 'Transmitting Patient Vitals Over a Reliable ZigBee Mesh Network'}
-#
-#for key in papers:
-#    print('%-50s citations = %5s' % (papers[key][:50], get_number(html, key, row_start='<tr class="gsc_a_tr">')))
-
-
-papers_html = get_chunk(html, '<tbody id="gsc_a_b">', '</tbody>')
-papers_html = papers_html.replace('<tr class="gsc_a_tr">', '')
-papers_html = papers_html.replace('</tr>', '\n')
-papers = papers_html[:-1].split('\n')
-hparser = HTMLParser()
 
 title_len = 70
 print_templ = '%-' + str(title_len) + 's %9s %-10s'
@@ -105,44 +32,31 @@ book_citations = 0
 impact_total = 0
 book_count = 0
 for paper in papers:
-    key = 'citation_for_view='
-    start = paper.find(key) + len('citation_for_view=')
-    end = start + paper[start:].find('"')
-    id = paper[start:end]
-
-    paper = [v for v in re.sub('<[^<]+?>', '|', paper).split('|') if v]
-    if len(paper) == 0:
-        continue
-
-    name = HTML.unescape(paper[0])
+    name = paper.title
     if len(name) > title_len:
         name = name[:(title_len-3)] + '...'
 
-    if len(paper) < 6:
-        count = 0
-    else:
-        try:
-            count = int(paper[4])
-        except:
-            count = 0
+    impact = ''
 
-    impact = get_if(paper[2])
-
-    a_book = is_book(paper[2])
-
-    if count == 0 and impact == '' and not a_book:
-        continue
-
-    if impact != '':
+    if paper.is_conference:
+        if paper.citations == 0:
+            continue
+        conf_citations += paper.citations
+    if paper.is_journal:
         journal_papers += 1
-        journal_citations += count
-    elif a_book:
-        book_count += 1
-        book_citations += count
-    else:
-        conf_citations += count
+        journal_citations += paper.citations
 
-    print(print_templ % (name[:title_len], format(count, ',d'), impact))
+        if paper.impact_factor != 'tbd':
+            impact = float(paper.impact_factor)
+            impact_total += impact
+            impact = '%10.03f' % (impact)
+        else:
+            impact = '%10s' % (paper.impact_factor)
+    elif paper.is_book:
+        book_count += 1
+        book_citations += paper.citations
+
+    print(print_templ % (name[:title_len], format(paper.citations, ',d'), impact))
 
 print(print_templ % ('-' * title_len, '-' * 9, '-' * 10))
 
