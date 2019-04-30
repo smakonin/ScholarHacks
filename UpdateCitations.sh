b902f780423fc5828752d9e478da220ec8f86553
Stephen Makonin <stephen@makonin.com>
2019-04-30 08:35:05 -0700

    smallish upd

--- UpdateCitations.sh
+++ UpdateCitations.sh
@@ -1,10 +1,14 @@
-cd /home/pi/ScholarHacks/
+
+#cd /home/pi/ScholarHacks/
+cd /Users/stephen/SourceCode/ScholarHacks
+
 cd ../smakonin.github.io/
 git pull
 cd ../ScholarHacks/
 ./GenerateCitationJSON.py
 cd ../smakonin.github.io/
 git add .
-git commit -m 'upd citation json' --quiet
-git push > /dev/null --quiet
+git status
+git commit -m 'upd citation json'
+git push > /dev/null
 cd ../ScholarHacks/
