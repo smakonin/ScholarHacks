1d3937279fd5204d238cd9ec6c81eda4e3bf9fa3
Stephen Makonin <stephen@makonin.com>
2021-02-04 09:12:08 -0800

    small-ish upds

--- UpdateCitations.sh
+++ UpdateCitations.sh
@@ -1,22 +1,28 @@
 
-head /Users/stephen/SourceCode/smakonin.github.io/scholar.js
+head ~/SourceCode/smakonin.github.io/scholar.js
 
 #cd /home/pi/ScholarHacks/
-cd /Users/stephen/SourceCode/ScholarHacks
+cd ~/SourceCode/ScholarHacks
 
+echo "*** INGESTING GOOGLE SCHOLAR DATA:"
 cd ../smakonin.github.io/
 git pull
 cd ../ScholarHacks/
 ./GenerateCitationJSON.py
 cd ../smakonin.github.io/
 
-python3 ./SourceCode/PySitemap/main.py --url="http://makonin.com/"
+echo "*** UPDATE WITH NEW CV:"
+cp -v ~/Documents/Job\ Docs/SMakonin_CV.pdf ./doc/
 
+#echo "*** BUILD NEW SITEMAP:"
+#python3 ~/SourceCode/PySitemap/main.py --url="http://makonin.com/"
 
+echo "*** PUSH WEBSITE UPDATES:"
 git add .
 git status
 git commit -m 'upd citation json'
 git push
 #git push > /dev/null
+
 head scholar.js
 cd ../ScholarHacks/
